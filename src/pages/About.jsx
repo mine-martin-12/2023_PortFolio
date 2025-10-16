75bcee856fc2695353a8e108c5a3005d09e8ac14
mine-martin-12 <martinmine22@gmail.com>
2025-10-16 20:42:51 +0300

    Fix Vercel deployment error

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -130,7 +130,7 @@ const About = () => {
               {/* experience */}
               <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
                 <div className="text-2xl xl:text-4xl font-extrabold text-indigo-500 mb-2">
-                  <CountUp start={0} end={2} duration={8} /> +
+                  <CountUp start={0} end={5} duration={8} /> +
                 </div>
                 <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
                   Years of experience
@@ -150,7 +150,7 @@ const About = () => {
               {/* Projects */}
               <div className="relative flex-1">
                 <div className="text-2xl xl:text-4xl font-extrabold text-indigo-500 mb-2">
-                  <CountUp start={0} end={15} duration={5} /> +
+                  <CountUp start={0} end={12} duration={5} /> +
                 </div>
                 <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
                   Finished Projects
