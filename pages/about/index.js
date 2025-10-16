ec48b25102baeefe07957c4e449c734ae14efe34
mine-martin-12 <martinmine22@gmail.com>
2025-10-16 20:33:02 +0300

    Fix Vercel deployment error

--- pages/about/index.js
+++ pages/about/index.js
@@ -157,7 +157,7 @@ const About = () => {
               {/* experience */}
               <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
                 <div className="text-2xl xl:text-4xl font-extrabold text-indigo-500 mb-2">
-                  <CountUp start={0} end={5} duration={8} /> +
+                  <CountUp start={0} end={5} duration={5} /> +
                 </div>
                 <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]:">
                   Years of experience
